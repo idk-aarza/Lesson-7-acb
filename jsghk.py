498dd0594c4246c6f103cd959dbff0da0fcd727f
idk-aarza <aarzak2012@gmail.com>
2024-10-26 22:40:47 +0530

    jsghk

---
+++ jsghk.py
@@ -0,0 +1,8 @@
+print('enter a character')
+c=input()
+if c>='a'and c<= "z":
+    print("\nlt is an alphabet")
+elif c>='A' and c<="z":
+    print('\nlt is an alphabet')
+else:
+    print('\nlt is not an alpabet!')
